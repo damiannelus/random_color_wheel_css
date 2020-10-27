8c094cf885e41cfb3f2ba98553b8bbead9ee838b
Damian Drewulski (damiannelus) <damiannelus@gmail.com>
2020-10-27 07:59:41 +0100

    added drawing a theme via drawComplementaryColors and themeprovider

--- color_wheel_drawer/src/App.tsx
+++ color_wheel_drawer/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import {ThemeProvider} from 'styled-components';
 import './App.css';
 import MagicButton from "./global";
+import drawComplementaryColors from "./helpers";
 
-const theme = {
+let theme = {
   background: "#1ECBE1",
   color: "#E1341E"
 };
@@ -14,6 +16,11 @@ function App() {
         <MagicButton theme={theme}>
           Dupa
         </MagicButton>
+        <ThemeProvider theme={drawComplementaryColors}>
+          <MagicButton>
+            drawComplementaryColors
+        </MagicButton>
+        </ThemeProvider>
       </header>
     </div>
   );
---
+++ color_wheel_drawer/src/helpers.tsx
@@ -0,0 +1,10 @@
+const drawComplementaryColors: () => object = function (
+): object {
+    console.log("drawComplementaryColors called");
+    return {
+        background: "#1ECBE1",
+        color: "#E1341E"
+      }
+}
+
+export default drawComplementaryColors;
