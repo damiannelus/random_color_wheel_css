025b19bfb11ae1b0d52b675cc678ee0aacb3d247
Damian Drewulski (damiannelus) <damiannelus@gmail.com>
2020-10-27 08:06:20 +0100

    colors are drawn and displayed. finding complementary color doesn't work

--- color_wheel_drawer/src/helpers.tsx
+++ color_wheel_drawer/src/helpers.tsx
@@ -1,9 +1,15 @@
 const drawComplementaryColors: () => object = function (
 ): object {
     console.log("drawComplementaryColors called");
+    let randomColor = Math.floor(Math.random()*16777215);
+    let complementaryColor = 0xffffff ^ randomColor;
+
+    console.log("drawn color: " + randomColor.toString(16));
+    console.log("complementary color: " + complementaryColor.toString(16));
+
     return {
-        background: "#1ECBE1",
-        color: "#E1341E"
+        background: "#"+complementaryColor.toString(16),
+        color: "#"+randomColor.toString(16)
       }
 }
 
