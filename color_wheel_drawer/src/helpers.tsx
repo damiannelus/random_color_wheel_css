0910e23867646a94fbbe685a83a3d27668cfc2d2
Damian Drewulski (damiannelus) <damiannelus@gmail.com>
2020-10-27 08:55:28 +0100

    proper color drawing implementation

--- color_wheel_drawer/src/helpers.tsx
+++ color_wheel_drawer/src/helpers.tsx
@@ -1,16 +1,54 @@
 const drawComplementaryColors: () => object = function (
 ): object {
     console.log("drawComplementaryColors called");
-    let randomColor = Math.floor(Math.random()*16777215);
-    let complementaryColor = 0xffffff ^ randomColor;
+    let randomColorRGB = {
+        r: Math.floor(Math.random()*255),
+        g: Math.floor(Math.random()*255),
+        b: Math.floor(Math.random()*255)
+    };
+    let complementaryColorRGB = {
+        r: 255 - randomColorRGB.r,
+        g: 255 - randomColorRGB.g,
+        b: 255 - randomColorRGB.b
+    };
 
-    console.log("drawn color: " + randomColor.toString(16));
-    console.log("complementary color: " + complementaryColor.toString(16));
+    let randomColor = RGBColorToHexColor(randomColorRGB);
+    let complementaryColor = RGBColorToHexColor(complementaryColorRGB);
+
+    console.log("drawn color: " + randomColor);
+    console.log("complementary color: " + complementaryColor);
 
     return {
-        background: "#"+complementaryColor.toString(16),
-        color: "#"+randomColor.toString(16)
+        background: complementaryColor,
+        color: randomColor
       }
 }
+
+interface ColorRGB {
+    r: number,
+    g: number,
+    b: number
+}
+
+// const RGBColorToHexColor: (RGBColor: object) => String = function (
+//     // RGBColor: object
+// ): String {
+//     return "#"+RGBtoHEX(RGBColor.r)+RGBtoHEX(RGBColor.g)+RGBtoHEX(RGBColor.b)
+// }
+
+const RGBColorToHexColor = function (RGBColor: ColorRGB): String {
+    return "#"+RGBtoHEX(RGBColor.r)+RGBtoHEX(RGBColor.g)+RGBtoHEX(RGBColor.b)
+}
+
+const RGBtoHEX: (RGB: number) => String = function (
+    RGB: number
+): String {
+  let hex = Number(RGB).toString(16);
+  if (hex.length < 2) {
+       hex = "0" + hex;
+  }
+  return hex;
+}
+
 
 export default drawComplementaryColors;
